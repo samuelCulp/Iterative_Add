33b821688a0c4609f4a235f9df6ce7165fdb29b9
samuelCulp <99611615+samuelCulp@users.noreply.github.com>
2022-04-15 13:10:31 -0500

    Update Recursion.cpp
    The problem is the while loop if the number is 20 it will overshoot the target because 15 + 6 = 21. The way to fix this is to set the target at 15 and then it will stop before the target. The problem that causes is if the use (0, 2) for the function it will stop at 14. So I made something ahead of add to stop it at 20 with any input I also stop the double display.

--- Recursion.cpp
+++ Recursion.cpp
@@ -1,5 +1,5 @@
 // Recursion.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// samuel culp
 
 #include <iostream>
 
@@ -7,12 +7,13 @@ int iterative_add(int, int);
 
 int main()
 {
-    std::cout << iterative_add(0, 1) << std::endl;
+    //std::cout << iterative_add(0, 1) << std::endl;
+     iterative_add(0, 1); // this so you don't see double
 }
 
 int iterative_add(int base, int add) {
     int result = base;
-
+    int future = add + base; // you can add this to be ahead of add
     do
     {
         result += add;
@@ -20,7 +21,8 @@ int iterative_add(int base, int add) {
         std::cout << result << std::endl;
 
         add++;
-    } while (result < 20);
-
+        future = result + add; // this is adding ahead of add
+    } while (future < 21); // If the number is 20 in the while loop it will overshoot the target goal because 15 + 6 = 21
+                            // to fix this you use 15 or have something ahead of add so it will stop at 20
     return result;
 }
